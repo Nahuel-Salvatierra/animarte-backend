81448d0801d2550be57d2c2640463167ef70e3c8
Nahuel <salvatierra.nahuel@gmai.com>
2023-12-03 16:30:07 -0300

    Fix globalPipes to validate auth

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Inject, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { create } from "domain";
+import { CreateUserDto } from "./createUser.dto";
+import { LoginUserDto } from "./loginUser.dto";
 
 @Controller('auth')
 export class AuthController{
@@ -8,12 +9,12 @@ export class AuthController{
     @Inject(AuthService) private readonly authService:AuthService
   ){}
     @Post('signup')
-    async singUp (@Body() createUserDto){
+    async singUp (@Body() createUserDto:CreateUserDto){
       return await this.authService.signUp(createUserDto)
     }
 
     @Post('login')
-    async login (@Body() loginUserDto) {
+    async login (@Body() loginUserDto:LoginUserDto) {
       return await this.authService.login(loginUserDto)
     }
 }
--- src/module/user/dto/createUser.dto.ts
+++
@@ -1,4 +0,0 @@
-import { OmitType } from '@nestjs/mapped-types';
-import { UserDto } from './user.dto';
-
-export class CreateUserDto extends OmitType(UserDto, ['id'] as const) {}
